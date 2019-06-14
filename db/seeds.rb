9920176825b342498ecf64a3edad72bd6c6d5e6e
Lena Nigrell <nigrell@live.se>
2019-06-14 16:11:59 +0200

    user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating users...'
+
+user_1 = User.create!(password: '123456',
+                      email: 'petra@ikea.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/pzyee',
+                      first_name: 'Petra',
+                      last_name: 'Erlandsson',
+                      car_plate: 'ABC 123')
+user_2 = User.create!(password: '123456',
+                      email: 'dennis@ikea.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/depersson',
+                      first_name: 'Dennis',
+                      last_name: 'Persson',
+                      car_plate: 'DEF 456')
+user_3 = User.create!(password: '123456',
+                      email: 'lena@ikea.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/leniglnn',
+                      first_name: 'Lena',
+                      last_name: 'Nigrell',
+                      car_plate: 'GHI 789')
+user_4 = User.create!(password: '123456',
+                      email: 'oscar@ikea.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/oscnil',
+                      first_name: 'Oscar',
+                      last_name: 'Nilestam',
+                      car_plate: 'CBA 321')
+user_5 = User.create!(password: '123456',
+                      email: 'robert@ikea.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/atmosfeer',
+                      first_name: 'Robert',
+                      last_name: 'Svensson',
+                      car_plate: 'FED 654')
+user_6 = User.create!(password: '123456',
+                      email: 'anton@ikea.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/atmosfeer',
+                      first_name: 'Anton',
+                      last_name: 'Svensson',
+                      car_plate: 'IHG 987')
