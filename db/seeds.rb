4d27df2e606ac9b9f173865f7e3519817c5f882a
Oscar Nilestam <oscarnilestam@gmail.com>
2019-06-17 10:43:10 +0200

    added charging posts, some puts notifications, fixed typo etc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Clearing all users'
+User.delete_all
+puts 'Users deleted!'
+
 puts 'Creating users...'
 
 user_1 = User.create!(password: '123456',
@@ -13,33 +18,57 @@ user_1 = User.create!(password: '123456',
                       first_name: 'Petra',
                       last_name: 'Erlandsson',
                       car_plate: 'ABC 123')
+puts 'User 1 created!'
+
 user_2 = User.create!(password: '123456',
                       email: 'dennis@ikea.com',
                       photo: 'https://kitt.lewagon.com/placeholder/users/depersson',
                       first_name: 'Dennis',
                       last_name: 'Persson',
                       car_plate: 'DEF 456')
+
+puts 'User 2 created!'
 user_3 = User.create!(password: '123456',
                       email: 'lena@ikea.com',
                       photo: 'https://kitt.lewagon.com/placeholder/users/leniglnn',
                       first_name: 'Lena',
                       last_name: 'Nigrell',
                       car_plate: 'GHI 789')
+puts 'User 3 created!'
+
 user_4 = User.create!(password: '123456',
                       email: 'oscar@ikea.com',
                       photo: 'https://kitt.lewagon.com/placeholder/users/oscnil',
                       first_name: 'Oscar',
                       last_name: 'Nilestam',
                       car_plate: 'CBA 321')
+puts 'User 4 created!'
+
 user_5 = User.create!(password: '123456',
                       email: 'robert@ikea.com',
                       photo: 'https://kitt.lewagon.com/placeholder/users/atmosfeer',
                       first_name: 'Robert',
-                      last_name: 'Svensson',
+                      last_name: 'Andersson',
                       car_plate: 'FED 654')
+puts 'User 5 created!'
+
 user_6 = User.create!(password: '123456',
                       email: 'anton@ikea.com',
                       photo: 'https://kitt.lewagon.com/placeholder/users/atmosfeer',
                       first_name: 'Anton',
                       last_name: 'Svensson',
                       car_plate: 'IHG 987')
+puts 'User 6 created!'
+puts 'All users created successfully!'
+
+puts 'Clearing all charging posts'
+ChargingPost.delete_all
+puts 'All charging posts deleted!'
+
+puts 'Creating charging posts'
+28.times { |index| ChargingPost.create!()
+  print "#{index + 1} , " }
+puts 'All charging posts created successfully!'
+
+puts 'Creating charging sessions'
+
