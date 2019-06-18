9df7be3dc366498200d7d46466876ef12b18574a
Lena Nigrell <nigrell@live.se>
2019-06-18 11:02:45 +0200

    array with avaialble posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,26 @@ class PagesController < ApplicationController
   def profile
     @user = current_user
     @session = @user.charging_sessions.find_by(end_time: nil)
+    @available_posts = all_posts - occupied_posts
   end
+end
+
+private
+
+def occupied_posts
+  array_occupied = []
+  occupied = ChargingSession.where(end_time: nil)
+  occupied.each do |post|
+    array_occupied << post.id
+  end
+  return array_occupied
+end
+
+def all_posts
+  array_all = []
+  all = ChargingPost.all
+  all.each do |post|
+    array_all << post.id
+  end
+  return array_all
 end
