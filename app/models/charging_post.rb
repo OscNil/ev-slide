24b3ca6205552def1a2b278308ca1f1f310a452e
Lena Nigrell <nigrell@live.se>
2019-06-20 11:22:31 +0200

    dropdown functionality better

--- app/models/charging_post.rb
+++ app/models/charging_post.rb
@@ -1,3 +1,10 @@
 class ChargingPost < ApplicationRecord
   has_many :charging_sessions
+  scope :available, -> { joins(:charging_sessions).where(charging_sessions: { end_time: nil } ) }
+
+  def self.available_posts
+    joins(:charging_sessions).where(charging_sessions: { end_time: nil } )
+  end
+
+
 end
