4ddb76795473d500979a78a9b66bfdfb96449968
Oscar Nilestam <oscarnilestam@gmail.com>
2019-06-24 11:09:43 +0200

    Dummy implementation only, hard coded values but visible

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,11 @@ Rails.application.routes.draw do
       root 'users/sessions#new', as: :unauthenticated_root
     end
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :charging_sessions, only: [:create, :update]
-  resources :queueings, only: [:create, :update]
+  resources :queueings, only: [:read, :create, :update]
 
   get "profile", to: 'pages#profile'
+  get "queue", to: 'pages#queue'
 end
