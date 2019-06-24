fc48a793368c5efc3494d7c5534b8b0c65c653b7
Lena Nigrell <nigrell@live.se>
2019-06-24 13:18:48 +0200

    cleaner code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,23 @@
 class PagesController < ApplicationController
   def profile
     @user = current_user
-    # initiate charging
-    @available_posts = ChargingPost.available_posts
-    @charging_session = ChargingSession.new
-    # ongoing charging
+
+    # 1. If the user is charging...
     @session = @user.charging_sessions.find_by(end_time: nil)
-    # need to queue
-    @queueing = Queueing.new
-    # ongoing queue
-    @my_ongoing_queueing = @user.queueings.find_by(end_time: nil)
+
+    # 2 + 3: If the user is in the queue...and if there is / is NOT a available post for the user
     @my_number_in_queue = @user.my_number_in_queue
-    # (ongoing queue and) parking slot is avaiable - your turn
+    @my_ongoing_queueing = @user.queueings.find_by(end_time: nil)
     @my_turn = current_user.myturn?
 
+    # If the user is NOT charging or NOT in queue..
+    # 4...and there is a queue or no available charger posts
+    # 5.. and charger post is available
     @no_queue = Queueing.no_queue?
+    @available_posts = ChargingPost.available_posts
+
+    # initiate
+    @charging_session = ChargingSession.new
+    @queueing = Queueing.new
   end
 end
