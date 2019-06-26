038e4ffe3d89cea1c507734aec67bbbf0932e6df
Lena Nigrell <nigrell@live.se>
2019-06-26 22:01:19 +0200

    adjustments to reload your card in the queue to also change to charging icon with number

--- app/javascript/modules/reload.js
+++ app/javascript/modules/reload.js
@@ -1,14 +1,21 @@
 function reload(){
+// Note: below is only reloading necessary info on your own card on Queue page
+// when it is your turn. If more people are in the queue, those won't be updated.
+// This is only a solution for the demo.
 
   setInterval(function(){
     $.ajax({ url: "/queue_refresh", success: function(data){
-        if(!data.myturn) return;
-        // console.log(data.userId)
-        // query class with correct user id
-        const myCard = document.querySelector(`.ev-card-${data.userId}`);
-        // add class of me-card-charger-available to correct element
-        myCard.classList.add('me-card-charger-available');
-      }, dataType: "json"});
-  }, 1000);
+      if(!data.myturn) return;
+
+      // query class to find card with correct user id
+      const myCard = document.querySelector(`.ev-card-${data.userId}`);
+      // add class of me-card-charger-available to make card green
+      myCard.classList.add('me-card-charger-available');
+      // query class to find charger/time info with correct user id
+      const myCharger = document.querySelector(`.ev-info-${data.userId}`);
+      // refresh that info to reflect correct icon and number of the charger
+      $(`.ev-info-${data.userId}`).load(location.href + ` .ev-info-${data.userId}`);
+    }, dataType: "json"});
+  }, 500);
 }
 export default reload;
