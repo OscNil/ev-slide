3a739c685ef97f8c7533019c8be9342113003fd0
Dennis Persson <depersson_81@telia.com>
2019-10-16 08:45:07 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,96 +24,96 @@ puts 'Users deleted!'
 puts 'Creating users...'
 
 user_1 = User.create!(password: '123456',
-                      email: 'petra@ikea.com',
-                      photo: 'https://kitt.lewagon.com/placeholder/users/pzyee',
-                      first_name: 'Petra',
-                      last_name: 'Elandersson',
+                      email: 'useremail@domain.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/username',
+                      first_name: 'First',
+                      last_name: 'Last',
                       car_plate: 'ABC 123',
                       points: 81)
 
 puts 'User 1 created!'
 
 user_2 = User.create!(password: '123456',
-                      email: 'dennis@ikea.com',
-                      photo: 'https://kitt.lewagon.com/placeholder/users/depersson',
-                      first_name: 'Dennis',
-                      last_name: 'Persson',
+                      email: 'useremail@domain.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/username',
+                      first_name: 'First',
+                      last_name: 'Last',
                       car_plate: 'DEF 456',
                       points: -22)
 
 puts 'User 2 created!'
 user_3 = User.create!(password: '123456',
-                      email: 'lena@ikea.com',
-                      photo: 'https://kitt.lewagon.com/placeholder/users/leniglnn',
-                      first_name: 'Lena',
-                      last_name: 'Nigrell',
+                      email: 'useremail@domain.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/username',
+                      first_name: 'First',
+                      last_name: 'Last',
                       car_plate: 'GHI 789',
                       points: 32)
 puts 'User 3 created!'
 
 user_4 = User.create!(password: '123456',
-                      email: 'oscar@ikea.com',
-                      photo: 'https://kitt.lewagon.com/placeholder/users/oscnil',
-                      first_name: 'Oscar',
-                      last_name: 'Nilestam',
+                      email: 'useremail@domain.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/username',
+                      first_name: 'First',
+                      last_name: 'Last',
                       car_plate: 'CBA 321',
                       points: 50)
 puts 'User 4 created!'
 
 user_5 = User.create!(password: '123456',
-                      email: 'robert@ikea.com',
-                      photo: 'https://kitt.lewagon.com/placeholder/users/atmosfeer',
-                      first_name: 'Robert',
-                      last_name: 'Andersson',
+                      email: 'useremail@domain.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/username',
+                      first_name: 'First',
+                      last_name: 'Last',
                       car_plate: 'FED 654',
                       points: -6)
 puts 'User 5 created!'
 
 user_6 = User.create!(password: '123456',
-                      email: 'anton@ikea.com',
-                      photo: 'https://kitt.lewagon.com/placeholder/users/atmosfeer',
-                      first_name: 'Nicholas',
-                      last_name: 'Feer',
+                      email: 'useremail@domain.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/username',
+                      first_name: 'First',
+                      last_name: 'Last',
                       car_plate: 'IHG 987',
                       points: -98)
 
 puts 'User 6 created!'
 
 user_7 = User.create!(password: '123456',
-                      email: 'alan@ikea.com',
-                      photo: 'https://kitt.lewagon.com/placeholder/users/alancaunt',
-                      first_name: 'Alan',
-                      last_name: 'Caunt',
+                      email: 'useremail@domain.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/username',
+                      first_name: 'First',
+                      last_name: 'Last',
                       car_plate: 'III 999',
                       points: 29)
 
 puts 'User 7 created!'
 
 user_8 = User.create!(password: '123456',
-                      email: 'gracy@ikea.com',
-                      photo: 'https://kitt.lewagon.com/placeholder/users/gracykantz',
-                      first_name: 'Gracy',
-                      last_name: 'Kantzabedian',
+                      email: 'useremail@domain.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/username',
+                      first_name: 'First',
+                      last_name: 'Last',
                       car_plate: 'NIT 851',
                       points: -70)
 
 puts 'User 8 created!'
 
 user_9 = User.create!(password: '123456',
-                      email: 'anne@ikea.com',
-                      photo: 'https://kitt.lewagon.com/placeholder/users/annemff',
-                      first_name: 'Anne',
-                      last_name: 'Hofmann',
+                      email: 'useremail@domain.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/username',
+                      first_name: 'First',
+                      last_name: 'Last',
                       car_plate: 'GGG 777',
                       points: 0)
 
 puts 'User 9 created!'
 
 user_10 = User.create!(password: '123456',
-                      email: 'drazen@ikea.com',
-                      photo: 'https://kitt.lewagon.com/placeholder/users/drazenv',
-                      first_name: 'Drazen',
-                      last_name: 'Vukovic',
+                      email: 'useremail@domain.com',
+                      photo: 'https://kitt.lewagon.com/placeholder/users/username',
+                      first_name: 'First',
+                      last_name: 'Last',
                       car_plate: 'AAA 111',
                       points: 0)
 
